dfe03a0a54d4837c0a12927f391707b063fe3c0e
Clifton Somers <csomers@purdue.edu>
2015-02-10 13:12:26 -0500

    all done, made and everything

--- traverse.c
+++ traverse.c
@@ -28,17 +28,32 @@ struct SNode {
 // ==============================================================
 // PROGRAM 
 
+// ==============================================================
+// STACK & NODE FUNCTION 
+struct Stack *	stack_create(void);
+void			stack_destroy(struct Stack * S);
+struct BST *	stack_pop(struct Stack * S, int * lf, int * rf);
+struct Stack *	stack_push(struct Stack * S, struct BST * bst);
 
+void			stack_flags(struct Stack * S, int lf, int rf);
+
+int				stack_isempty(struct Stack * S);
+struct SNode *	stack_node_create(struct BST *);
+void			stack_node_destroy(struct SNode * S);
 // ==============================================================
 // BST FUNCTIONS
 struct BST *	tree_combine_with(struct BST ** lc, struct BST ** rc, char c);
 struct BST *	tree_plant(char c);
 void			tree_chop_down(struct BST * vic);
-void			tree_visit(struct BST * node);
+char	 		tree_visit(struct BST * node);
 void			tree_print(struct BST * root, int depth);
 void			tree_prefix(struct BST * node);
 void			tree_infix(struct BST * node);
 void			tree_postfix(struct BST * node);
+
+
+// ==============================================================
+// BST FUNCTIONS
 
 // COMBINE
 /* This function can be implemented a number of ways. In this case, it is both
@@ -58,6 +73,8 @@ struct BST * tree_plant(char c) {
 	struct BST * root = malloc(sizeof(struct BST));
 	if(!root) return NULL;
 	root->val = c;
+	root->left = NULL;
+	root->right = NULL;
 	return(root);
 }
 // DEALLOC
@@ -69,8 +86,9 @@ void tree_chop_down(struct BST * vic) {
 	}
 }
 // PRINT NODE
-void tree_visit(struct BST * node) {
-	printf("%c", node->val);
+char tree_visit(struct BST * node) {
+	//printf("%c", node->val);
+	return(node->val);
 }
 // PRINT TREE
 void tree_print(struct BST * root, int depth) {
@@ -85,6 +103,13 @@ void tree_print(struct BST * root, int depth) {
 		tree_print(root->right, depth + 1);
 	}
 }
+// COUNT NODES
+int tree_count(struct BST * bst) {
+	if(bst) {
+		return(tree_count(bst->left) + tree_count(bst->right) + 1);
+	}
+	return 0;
+}
 
 
 /* --------------------------------------------------------------\
@@ -93,65 +118,85 @@ void tree_print(struct BST * root, int depth) {
  *		representations of an expression tree. It is to do this 
  *		with a stack, *non-recursively*. 
 \* -------------------------------------------------------------*/
+int check_visit(int how, int ldone, int rdone, struct BST * node) {
+	if(!node) return(-1000);
+	if((!node->left && !node->right)||
+	(how == -1 && !ldone && !rdone) ||
+	(how ==  0 &&  ldone && !rdone) ||
+	(how ==  1 &&  ldone &&  rdone) ){
+		return 1;
+	}
+	return 0;	
+}
+
+void tree_traverse(struct BST * root, int how) {
+	// important objects
+	struct BST * node = root;
+	struct Stack * STACK = stack_create();
+	// counters and flags
+	int count = tree_count(root);
+	char * expression = malloc(sizeof(char) * (1 + count));
+	expression[count] = '\0';
+	int ldone = 0;
+	int rdone = 0;
+	int letter = 0;
+	// PROCESS
+	while(!(node == root && rdone == 1 && ldone == 1)) {//(count--) {
+		if(node) {
+			// VISIT CONDITIONS
+			if(check_visit(how, ldone, rdone, node)) { 
+				if(letter < count) {
+					expression[letter++] = tree_visit(node);
+				}
+			}
+			// TRAVEL CONDITONS
+			if((node->left || node->right) && (!ldone || !rdone)){
+				if(!ldone && node->left) {
+					stack_push(STACK, node);					
+					stack_flags(STACK, 1, 0);
+					node = node->left;
+					ldone = 0; 
+					rdone = 0;
+				}
+				else if(!rdone && node->right) {
+					stack_push(STACK, node);
+					stack_flags(STACK, 1, 1);
+					node = node->right;
+					ldone = 0; 
+					rdone = 0;
+				}
+			}// RETURN CONDITIONS
+			else {
+				node = stack_pop(STACK, &ldone, &rdone);		
+			}
+		}
+	}
+	// TROUBLE SHOOT
+	printf("%s\n", expression);
+	// DEALLOCATE
+	stack_destroy(STACK);
+	free(expression);
+}
+
 // TRAVERSE [N__LR]
 void tree_prefix(struct BST * root) {
+	printf("Prefix  = ");
+	tree_traverse(root, -1);
 }
 // TRAVERSE [L_N_R]
 void tree_infix(struct BST * root) {
-	// important objects
-	struct BST * node = root;
-	struct Stack * STACK = stack_create;
-
-	// counters and flags
-	int count = 20;
-	int ldone = 0;
-	int rdone = 0;
-	// process
-	while(count--) {
-		// VISIT CONDITIONS
-		if((!node->left && !node->right)  ||
-		   (ldone && !rdone)) {
-			tree_visit(node);
-		}
-		// TRAVEL CONDITONS
-		if(node->left || node->right) {
-			if(!ldone && node->left) {
-				stack_push(STACK, node);					
-				stack_flags(STACK, 1, 0);
-				node = node->left;
-				ldone = 1;
-			}
-			if(!lflag && node->right) {
-				stack_push(STACK, node);
-				stack_flags(STACK, 1, 0);
-				node = node->right;
-				rdone = 1;
-			}
-		}// RETURN CONDITIONS
-		else {
-			node = stack_pop(STACK);
-			rflag++;
-	}
-	stack_destroy(STACK);
+	printf("Infix   = ");
+	tree_traverse(root, 0);
 }
 // TRAVERSE [LR__N]
 void tree_postfix(struct BST * root) {
+	printf("Postfix = ");
+	tree_traverse(root, 1);
 }
+
+
 // ==============================================================
 // STACK & NODE FUNCTION 
-struct Stack *	stack_create(void);
-void			stack_destroy(struct Stack * S);
-struct BST *	stack_pop(struct Stack * S);
-struct BST *	stack_pull(struct Stack * S);
-struct Stack *	stack_push(struct Stack * S, struct BST * bst);
-
-void			stack_flags(struct Stack * S, int lf, int rf);
-
-int				stack_isempty(struct Stack * S);
-struct SNode *	stack_node_create(struct BST *);
-void			stack_node_destroy(struct SNode * S);
-
-
 
 // ISEMPTY
 int	stack_isempty(struct Stack * S) {
@@ -174,6 +219,8 @@ void stack_destroy(struct Stack * S) {
 struct SNode * stack_node_create(struct BST * bst) {
 	struct SNode * nsn = malloc(sizeof(struct SNode));
 	nsn->ret = bst;
+	nsn->ldone = 0;
+	nsn->rdone = 0;
 	nsn->next = NULL;
 	return(nsn);
 }
@@ -186,21 +233,24 @@ void stack_node_destroy(struct SNode * sn) {
 	}
 }
 // POP NODE
-struct BST * stack_pop(struct Stack * S) {
+struct BST * stack_pop(struct Stack * S, int * L, int * R) {
+	// assign and collect
 	if(stack_isempty(S)) return NULL;
 	struct SNode * old = S->sn; 
-	struct BST * contents = old->ret;// = S->sn;
+	struct BST * contents = S->sn->ret;
+	*L = S->sn->ldone;
+	*R = S->sn->rdone;
+	// sever and return
 	S->sn = old->next;
 	old->next = NULL;
 	old->ret = NULL;
 	free(old);
-	//stack_node_destroy(old);
 	return(contents);
 }
 // PULL NODE
-struct BST * stack_pull(struct Stack * S) {
+struct BST * stack_pull(struct Stack * S, int * L, int * R) {
 	if(stack_isempty(S)) return NULL;
-	return(S->sn);	
+	return(S->sn->ret);	
 }
 // PUSH NODE
 struct Stack * stack_push(struct Stack * S, struct BST * bst) {
@@ -220,9 +270,12 @@ struct BST * function_tree(int tn);
 void		 function_line(int ln);
 
 int main(int argc, char * argv[]) {
+	// [0] Validate and Parse
 	if(argc >= 1) printf("%s ", argv[0]);
 	if(argc >= 2) printf("%s ", argv[1]);
 	if(argc != 2 || !isdigit(argv[1][0])) return EXIT_FAILURE;
+
+	// [1] Build Expression Tree in Question
 	int tn = atoi(argv[1]);
 	struct BST * tree = function_tree(tn);	
 	function_line(1);
@@ -230,6 +283,14 @@ int main(int argc, char * argv[]) {
 	function_line(0);
 	if(!tree) return EXIT_FAILURE;
 
+	// [2] Perform Tree Traversals
+	printf("\n");
+	tree_prefix(tree);
+	tree_infix(tree);
+	tree_postfix(tree);
+
+	// [3] Deallocate and Return
+	tree_chop_down(tree);
 
 	return EXIT_SUCCESS ;
 }
@@ -255,7 +316,7 @@ struct BST * function_tree(int tn) {
 		Y = tree_plant('A');
 		Z = tree_combine_with(&X, &Y, '+');
 		X = tree_plant('C');
-		Y = tree_combine_with(&X, &Z, '$');
+		Y = tree_combine_with(&Z, &X, '$');
 
 		X = tree_combine_with(&W, &Y, '-');
 	}
